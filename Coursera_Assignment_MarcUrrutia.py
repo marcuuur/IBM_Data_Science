f57a4c529704476d3895104f797b7ac45aa35e0a
Marc <97913218+marcuuur@users.noreply.github.com>
2022-11-14 11:03:36 +0100

    Add files via upload
    Add Coursera final assignment on Tools for Data Science

---
+++ Coursera_Assignment_MarcUrrutia.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Marc Urrutia** <br>
+# Market Research & Insights Analyst
+
+# *I am interested in data science because I love to gain insights from data.*
+
+# ### The following code is an approximation to calculate the golden ratio.
+
+# In[5]:
+
+
+ratio = (1+5**(1/2))/2
+print (ratio)
+
+
+# #### Check my  __[LinkedIn profile](https://www.linkedin.com/in/marc-urrutia-61a924194/)__ :)
+# ---
+# - One
+# - Two
+# - Three
+# - Four
+# - Five
+# ---
+# 
+# | Brand | Model | Year |
+# | ----- | ----- | ----- |
+# | Dodge | Charger | 1970 |
+# | Lamborghini | Veneno | 2013 |
+# | Ferrari | 250 GTO | 1962 |
+# | Ferrari | Testarossa | 1984 |
+# | Lamborghini | Huracán Performante | 2016 |
